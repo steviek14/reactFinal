e367ff35fdcf89288400ccff3c0dcf38e3a13887
steviek14 <you@example.com>
2024-08-01 15:10:26 -0500

    finished app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Get the root element from the DOM
+const container = document.getElementById('root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Create a root and render the App component
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
